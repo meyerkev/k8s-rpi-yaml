6ecc75f8a4104348368b777d74000e4d3aba16f0
Kevin Meyer <meyerkev248@gmail.com>
2021-04-05 23:47:13 -0400

    My first ansible playbook

--- configuration/os_config/to_be_ansible.sh
+++ configuration/os_config/to_be_ansible.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
 UBUNTU_USER=ubuntu
-UBUNTU_DEFAULT_HOSTNAME=ubuntu.local
-
-# On your laptop
-ssh ${UBUNTU_USER?}@${UBUNTU_DEFAULT_HOSTNAME?}
-ssh-copy-id ubuntu@${UBUNTU_DEFAULT_HOSTNAME?}
-
-# On the box
-ssh ${UBUNTU_USER?}@${UBUNTU_DEFAULT_HOSTNAME?}
-mkdir -p ~/.kube/
-scp ~/.kube/config ${UBUNTU_USER?}@${UBUNTU_DEFAULT_HOSTNAME?}:~/.kube/
-
-# Python 3.8 for now
-sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo apt-get install -y python3 python3-distutils python-is-python3
-
-# Pip
-export PATH=${PATH}:${HOME}/.local/bin
-echo 'export PATH=${PATH}:${HOME}/.local/bin' >> ~/.bashrc
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --user
-
-pip install ansible
-pip install ipython
-
-
 # Things that can be moved to ansible:
 
 # New apt repos
@@ -34,8 +8,10 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+# Gotta do this manually on arm
+# curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+# sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
 # Install packages
 sudo apt-get update && sudo apt-get full-upgrade -y
@@ -66,7 +42,7 @@ sudo cat > /etc/docker/daemon.json <<EOF
 }
 EOF
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
