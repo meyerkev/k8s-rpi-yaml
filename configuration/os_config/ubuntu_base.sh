87f8bdd9e578fb9ccbb38423e4affd4a9747b029
Kevin Meyer <meyerkev248@gmail.com>
2021-04-06 22:46:14 -0400

    We now have an ansible playbook to install everything a new box could want

--- configuration/os_config/ubuntu_base.sh
+++ configuration/os_config/ubuntu_base.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+    echo "MUST PROVIDE A NEW HOSTNAME"
+    exit -1
+fi
+NEW_HOSTNAME=$1
 UBUNTU_USER=ubuntu
 UBUNTU_DEFAULT_HOSTNAME=ubuntu.local
 
@@ -8,21 +13,4 @@ ssh ${UBUNTU_USER?}@${UBUNTU_DEFAULT_HOSTNAME?}
 ssh-copy-id ubuntu@${UBUNTU_DEFAULT_HOSTNAME?}
 
 # On the box
-ssh ${UBUNTU_USER?}@${UBUNTU_DEFAULT_HOSTNAME?}
-mkdir -p ~/.kube/
-scp ~/.kube/config ${UBUNTU_USER?}@${UBUNTU_DEFAULT_HOSTNAME?}:~/.kube/
-
-# Python 3.8 for now
-sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo apt-get install -y python3 python3-distutils python-is-python3
-
-# Pip
-export PATH=${PATH}:${HOME}/.local/bin
-echo 'export PATH=${PATH}:${HOME}/.local/bin' >> ~/.bashrc
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py --user
-
-pip install ansible
-pip install ipython
-
+ssh ${UBUNTU_USER?}@${UBUNTU_DEFAULT_HOSTNAME?} "sudo hostnamectl set-hostname ${NEW_HOSTNAME?}; sudo reboot now"
